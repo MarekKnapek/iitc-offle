ec7447c71eb987e18327a15f78a9a825474720a2
MarekKnapek <knapek.mar@gmail.com>
2015-09-30 14:42:04 +0200

    New version: 0.6.2.

--- iitc-offle.user.js
+++ iitc-offle.user.js
@@ -2,7 +2,7 @@
 // @id         	iitc-plugin-offle
 // @name       	IITC plugin: offle
 // @category   	Misc
-// @version    	0.6.1
+// @version    	0.6.2
 // @namespace  	https://github.com/vrabcak/iitc-offle
 // @updateURL  	https://github.com/MarekKnapek/iitc-offle/raw/master/iitc-offle.user.js
 // @downloadURL	https://github.com/MarekKnapek/iitc-offle/raw/master/iitc-offle.user.js
